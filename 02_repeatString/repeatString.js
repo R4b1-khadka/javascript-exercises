3becdfe061b51d460b7fd7dd341eaccfc688cba6
rabi khadka <rabikhadka888@gmail.com>
2024-07-30 09:24:47 +0545

    update: fix all the problem in this exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,17 @@
 const repeatString = function (StringValue, repetition) {
-  let result = StringValue.repeat(repetition);
-  return result;
+  if (repetition < 0) return "ERROR";
+  else {
+    let result = StringValue.repeat(repetition);
+    return result;
+  }
 };
 repeatString("hey", 3);
+repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("goodbye", -1);
+const number = Math.floor(Math.random() * 1000);
+repeatString("odin", number);
+repeatString("", 10);
 // Do not edit below this line
 module.exports = repeatString;
