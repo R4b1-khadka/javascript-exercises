12ad2e3b13303319fa926ad124b453d644d1ff67
rabi khadka <rabikhadka888@gmail.com>
2024-07-30 09:05:23 +0545

    fix: repeat string problem return heyheyhey

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function (StringValue, repetition) {
+  let result = StringValue.repeat(repetition);
+  return result;
 };
-
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
