abe00c07187b7439a3a1fa4040d3456542913de2
rabi khadka <rabikhadka888@gmail.com>
2024-07-31 16:17:34 +0545

    pass 1 test of removeFromString

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (givenArray, remove) {
+  let newArray = [];
+  for (let i = 0; i < givenArray.length; i++) {
+    if (givenArray[i] !== remove) {
+      newArray.push(givenArray[i]);
+    }
+  }
+  return newArray;
 };
-
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
