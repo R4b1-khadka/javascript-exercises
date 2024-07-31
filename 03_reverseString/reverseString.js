230be0a63a783097f247778cbc9a803e5d9a8a11
rabi khadka <rabikhadka888@gmail.com>
2024-07-31 15:05:06 +0545

    javascript solution 04 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let splitString = str.split("");
+  let reversedString = splitString.reverse();
+  let joinReverse = reversedString.join("");
+  return joinReverse;
 };
-
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc! Hello, Odinite.");
+reverseString("");
 // Do not edit below this line
 module.exports = reverseString;
