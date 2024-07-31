33136a74426534de97b7355c7967158397802b6e
rabi khadka <rabikhadka888@gmail.com>
2024-07-31 16:18:14 +0545

    quick save

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,19 @@
+// const reverseString = function (str) {
+//   let splitString = str.split("");
+//   let reversedString = splitString.reverse();
+//   let joinReverse = reversedString.join("");
+// //   or you can chain them all together return str.split("").reverse.Join("");
+//   return joinReverse;
+
+//    string reverse using for loop
 const reverseString = function (str) {
-  let splitString = str.split("");
-  let reversedString = splitString.reverse();
-  let joinReverse = reversedString.join("");
-  return joinReverse;
+  let stringLength = str.length();
+  for (let i = stringLength; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
 };
+// };
 reverseString("hello");
 reverseString("hello there");
 reverseString("123! abc! Hello, Odinite.");
