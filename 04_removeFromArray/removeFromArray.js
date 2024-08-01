c83451f54befe3efe1145555b9e73a308b7af9d1
rabi khadka <rabikhadka888@gmail.com>
2024-08-01 09:20:45 +0545

    copied the solution from solution section

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,19 @@
-const removeFromArray = function (givenArray, remove) {
+const removeFromArray = function (givenArray, ...remove) {
   let newArray = [];
-  for (let i = 0; i < givenArray.length; i++) {
-    if (givenArray[i] !== remove) {
-      newArray.push(givenArray[i]);
+  givenArray.forEach((item) => {
+    if (!remove.includes(item)) {
+      newArray.push(item);
     }
-  }
+  });
   return newArray;
 };
 removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
