ffec4a9d7c1834baa5d29dcc2a04e19504871a59
rabi khadka <rabikhadka888@gmail.com>
2024-08-11 20:58:30 +0545

    update: my solution for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  celsiusScale = (5 / 9) * (temp - 32);
+  let rounded = Math.round(celsiusScale * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  FahrenheitScale = 1.8 * celsius + 32;
+  let roundedUp = Math.round(FahrenheitScale * 10) / 10;
+  return roundedUp;
 };
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+// convertToFahrenheit
+// convertToFahrenheit
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
