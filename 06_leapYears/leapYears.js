e0dfd5679723f3ad4af21b92d39bee25ee6c1bc8
rabi khadka <rabikhadka888@gmail.com>
2024-08-11 19:28:24 +0545

    update: leap year exercise my solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (getYear) {
+  if (getYear % 100 == 0) {
+    if (getYear % 400 == 0) return true;
+    else return false;
+  } else if (getYear % 4 == 0) return true;
+  else return false;
 };
-
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 // Do not edit below this line
 module.exports = leapYears;
